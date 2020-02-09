c52a35fabfda336bf50cf287e09a98fc1d39ac8b
Khalid T <khalid.tissire@gmail.com>
2020-02-09 05:24:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,13 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  @@all = []
   GENRES = []
   def initialize(title,genre)
     @title = title
     @genre = genre
     GENRES << @genre
+    @@all << self
   end
   
   def turn_page
